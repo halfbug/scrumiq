eb898ddce64ddd6d4c4214d57aadf51813320905
Sadaf <sadaf_cu@hotmail.com>
2025-08-06 13:28:44 +0500

    fixed initalize

--- core/initialize.py
+++ core/initialize.py
@@ -4,7 +4,7 @@ from core.config import config
 def set_environment_variables():
     """Set environment variables with fallback values and validation."""
     required_vars = {
-        'OPENAI_API_KEY': config.OPENAI_API_KEY,
+        # 'OPENAI_API_KEY': config.OPENAI_API_KEY,
         'PINECONE_API_KEY': config.PINECONE_KEY,
         'PINECONE_INDEX_NAME': config.PINECONE_INDEX_NAME,
         'API_KEY': config.API_KEY,
@@ -18,10 +18,10 @@ def set_environment_variables():
 
     # Set optional variables
     optional_vars = {
-        'GOOGLE_API_KEY': config.GOOGLE_API_KEY,
+        # 'GOOGLE_API_KEY': config.GOOGLE_API_KEY,
         'MONGO_URI': config.MONGO_URI,
-        'HUGGINGFACEHUB_API_TOKEN': config.HUGGINGFACEHUB_API_TOKEN,
-        'DEEPSEEK_API_KEY': config.DEEPSEEK_API_KEY,
+        # 'HUGGINGFACEHUB_API_TOKEN': config.HUGGINGFACEHUB_API_TOKEN,
+        # 'DEEPSEEK_API_KEY': config.DEEPSEEK_API_KEY,
         'MONGO_DB': config.MONGO_DB,
         'SEARCH_URL': config.SEARCH_URL,
     }
